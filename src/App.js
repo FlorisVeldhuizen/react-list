513f5e5bb1c3b86e8939e2b81a4027cfc05e192b
Floris Veldhuizen <floris.veldhuizen@nedap.com>
2020-05-18 10:53:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [listItems, setListItems] = useState(["test","test2"]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const added = e.target.elements.whycantInamethisitem.value;
+    setListItems(listItems.concat(added));
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <List listItems={listItems} setListItems={setListItems}/>
+      <AddItems handleSubmit={handleSubmit} />
     </div>
   );
+}
+
+function List(props){
+  return (
+    <div>
+      {props.listItems && props.listItems.map((item,index) => {
+          return <ListItem key={item+index} content={item} />
+        })
+      }
+    </div>
+  )
+}
+
+function ListItem(props){
+  return(
+    <div>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
+function AddItems(props){
+  return(
+    <form onSubmit={props.handleSubmit}>
+      <input type="text" name="whycantInamethisitem"/>
+      <button type="submit"> Add item</button>
+    </form>
+  )
 }
 
 export default App;
